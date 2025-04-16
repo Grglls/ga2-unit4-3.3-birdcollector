ba2a8f0545f9311246889b29d157e871cf149e9b
Greg Ells <gregells@gmail.com>
2025-04-16 18:29:51 -0400

    Add birds index page

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+# Temporary data:
+birds = [
+  {'name': 'Lolo', 'breed': 'Moa', 'description': 'Big scary bird!', 'age': 5},
+  {'name': 'Sachi', 'breed': 'Kiwi', 'description': 'Gentle and loving.', 'age': 2},
+  {'name': 'Bob', 'breed': 'Sparrow', 'description': 'Tinee-tiny bird.', 'age': 0},
+]
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
@@ -7,3 +14,7 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+
+def birds_index(request):
+    return render(request, 'birds/index.html', { 'birds': birds })

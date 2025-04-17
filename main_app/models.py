554a49c63d586cf17cfebfef8fefded9e623ca85
Greg Ells <gregells@gmail.com>
2025-04-17 16:52:25 -0400

    Add __str__ method to Bird model

--- main_app/models.py
+++ main_app/models.py
@@ -6,3 +6,6 @@ class Bird(models.Model):
     species = models.CharField(max_length=100)
     description = models.TextField()
     age = models.IntegerField()
+
+    def __str__(self):
+        return self.name

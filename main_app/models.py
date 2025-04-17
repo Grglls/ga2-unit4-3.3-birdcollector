a1524b6ca2988b24fda502fea6ff0ec9efeb9ddd
Greg Ells <gregells@gmail.com>
2025-04-17 16:42:53 -0400

    Add Bird model and make migrations

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Bird(models.Model):
+    name = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    description = models.TextField()
+    age = models.IntegerField()

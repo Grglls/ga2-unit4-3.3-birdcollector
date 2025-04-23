908959ee6e74e6353b65e4ebd4d18d1831bb1552
Greg Ells <gregells@gmail.com>
2025-04-23 11:10:18 -0400

    Install and configure the .env dependency

--- birdcollector/settings.py
+++ birdcollector/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+# Import and configure the django-environ package:
+import environ
+environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 

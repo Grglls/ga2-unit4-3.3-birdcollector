b131a786b5d3cc78690065c89ddd800c212e08ab
Greg Ells <gregells@gmail.com>
2025-04-23 14:10:14 -0400

    Register Photo model to admin portal

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Bird, Feeding, Toy
+from .models import Bird, Feeding, Toy, Photo
 
 # Register your models here.
 admin.site.register(Bird)
 admin.site.register(Feeding)
 admin.site.register(Toy)
+admin.site.register(Photo)

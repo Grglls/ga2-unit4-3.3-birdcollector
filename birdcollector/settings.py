c4709ea9ed33b7dc3957747ac75ebadc9a262006
Greg Ells <gregells@gmail.com>
2025-04-25 14:29:51 -0400

    Add login.html template and set default redirect URL

--- birdcollector/settings.py
+++ birdcollector/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Specify the default redirect URL after successful login:
+LOGIN_REDIRECT_URL = '/birds/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
